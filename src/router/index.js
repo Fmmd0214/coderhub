4589e1b82bcc196da4353b2621803ded6da023ee
Youli <ColaWyc@163.com>
2023-08-23 18:10:30 +0800

    补充fs模块API用法

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,15 @@ const fs = require('fs')
 
 function registerRouter(app) {
     // 1. 读取当前文件夹下的所有文件
+    /***
+     * fs.readdirSync(path[, options])
+     * 用于同步读取目录的内容。返回一个不包括 '.' 和 '..' 的文件名的数组。
+     * 返回一个数组
+     *
+     * __dirname
+     * 用来动态捕获当前文件模块所属目录的接对路径
+     */
     const files = fs.readdirSync(__dirname)
-    console.log(files)
 
     for (let file of files) {
         if (!file.endsWith('.router.js')) continue
